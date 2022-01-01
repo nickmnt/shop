3dafa24c589cb7473192310cfe85bee19c6365b6
nimamt <mtaghavinima@gmail.com>
2022-01-01 21:34:12 +0330

    API Skeleton initial

---
+++ Domain/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
