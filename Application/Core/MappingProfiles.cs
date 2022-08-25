7ddc80ca0fc218711433772fc58e245c4b6634d3
Devin <mtaghavinima@gmail.com>
2022-08-25 09:42:40 -0400

    Bug fix

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,8 +1,15 @@
-﻿using AutoMapper;
+﻿using Application.DTOs;
+using AutoMapper;
+using Domain;
 
 namespace Application.Core
 {
     public class MappingProfiles : Profile
     {
+        public MappingProfiles()
+        {
+            CreateMap<CreateProductDto, Product>();
+            CreateMap<UpdateProductDto, Product>();
+        }
     }
 }
