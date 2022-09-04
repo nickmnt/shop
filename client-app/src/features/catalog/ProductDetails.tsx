b284923a8c7e3b88ed6b1cd3c7a90f8308d62bd1
Devin <mtaghavinima@gmail.com>
2022-09-04 08:25:11 -0400

    Prevent buying products not in stock

--- client-app/src/features/catalog/ProductDetails.tsx
+++ client-app/src/features/catalog/ProductDetails.tsx
@@ -1,6 +1,8 @@
 import LoadingButton from '@mui/lab/LoadingButton/LoadingButton';
+import Button from '@mui/material/Button/Button';
 import Divider from '@mui/material/Divider/Divider';
 import Grid from '@mui/material/Grid/Grid';
+import Paper from '@mui/material/Paper/Paper';
 import Table from '@mui/material/Table/Table';
 import TableBody from '@mui/material/TableBody/TableBody';
 import TableCell from '@mui/material/TableCell/TableCell';
@@ -49,64 +51,74 @@ export default function ProductDetails() {
     if (!product) return <div>Not Found</div>;
 
     return (
-        <Grid container spacing={6} sx={{ marginTop: '5rem' }}>
-            <Grid item xs={6}>
-                <img src={product.pictureUrl} alt={product.name} style={{ width: '100%' }} />
-            </Grid>
-            <Grid item xs={6}>
-                <Typography variant="h3" color="textPrimary">
-                    {product.name}
-                </Typography>
-                <Divider sx={{ mb: 2 }} />
-                <Typography variant="h4" color="secondary">
-                    ${(product.price / 100).toFixed(2)}
-                </Typography>
-                <TableContainer>
-                    <Table>
-                        <TableBody>
-                            <TableRow>
-                                <TableCell>Name</TableCell>
-                                <TableCell>{product.name}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>Description</TableCell>
-                                <TableCell>{product.description}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>Type</TableCell>
-                                <TableCell>{product.type}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>Brand</TableCell>
-                                <TableCell>{product.brand}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>Quantity in stock</TableCell>
-                                <TableCell>{product.quantityInStock}</TableCell>
-                            </TableRow>
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <Grid container spacing={2} sx={{ marginTop: '1rem' }}>
-                    <Grid item xs={6}>
-                        <TextField variant="outlined" type="number" label="Quantity in Cart" fullWidth value={quantity} onChange={handleInputChange} />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <LoadingButton
-                            disabled={item?.quantity === quantity}
-                            loading={status.includes('pending')}
-                            onClick={handleUpdateCart}
-                            sx={{ height: '55px' }}
-                            color="primary"
-                            size="large"
-                            variant="contained"
-                            fullWidth
-                        >
-                            {item ? 'Update Quantity' : 'Add to Cart'}
-                        </LoadingButton>
+        <Paper sx={{ margin: '2rem', padding: '2rem' }}>
+            <Grid container spacing={6}>
+                <Grid item xs={6}>
+                    <img src={product.pictureUrl} alt={product.name} style={{ width: '100%' }} />
+                </Grid>
+                <Grid item xs={6}>
+                    <Typography variant="h3" color="textPrimary">
+                        {product.name}
+                    </Typography>
+                    <Divider sx={{ mb: 2 }} />
+                    <Typography variant="h4" color="secondary">
+                        ${(product.price / 100).toFixed(2)}
+                    </Typography>
+                    <TableContainer>
+                        <Table>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell>Name</TableCell>
+                                    <TableCell>{product.name}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Description</TableCell>
+                                    <TableCell>{product.description}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Type</TableCell>
+                                    <TableCell>{product.type}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Brand</TableCell>
+                                    <TableCell>{product.brand}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Quantity in stock</TableCell>
+                                    <TableCell>{product.quantityInStock}</TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <Grid container spacing={2} sx={{ marginTop: '1rem' }}>
+                        {product.quantityInStock! > 0 ? (
+                            <>
+                                <Grid item xs={6}>
+                                    <TextField variant="outlined" type="number" label="Quantity in Cart" fullWidth value={quantity} onChange={handleInputChange} />
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <LoadingButton
+                                        disabled={item?.quantity === quantity}
+                                        loading={status.includes('pending')}
+                                        onClick={handleUpdateCart}
+                                        sx={{ height: '55px' }}
+                                        color="primary"
+                                        size="large"
+                                        variant="contained"
+                                        fullWidth
+                                    >
+                                        {item ? 'Update Quantity' : 'Add to Cart'}
+                                    </LoadingButton>
+                                </Grid>
+                            </>
+                        ) : (
+                            <Button disabled fullWidth color="primary" size="large" variant="contained">
+                                Item not in stock
+                            </Button>
+                        )}
                     </Grid>
                 </Grid>
             </Grid>
-        </Grid>
+        </Paper>
     );
 }
